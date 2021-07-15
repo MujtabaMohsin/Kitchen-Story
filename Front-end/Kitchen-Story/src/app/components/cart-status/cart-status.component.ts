058d5522e2a12b082cf9b060a5ff19ea1fc0e88c
Muj <mmjj1010@gmail.com>
2021-07-15 03:18:27 +0300

    Initial commit

---
+++ Front-end/Kitchen-Story/src/app/components/cart-status/cart-status.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+ 
+@Component({
+  selector: 'app-cart-status',
+  templateUrl: './cart-status.component.html',
+  styleUrls: ['./cart-status.component.css']
+})
+export class CartStatusComponent implements OnInit {
+   
+  
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+ 
+
+}
